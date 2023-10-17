dd66c3a8de4e87920fc79a0ec7e5f3c17c1b972a
AYUSH <65271275+ayushishu@users.noreply.github.com>
2023-10-17 23:32:35 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -26,7 +26,7 @@ const config = {
 
 
 serviceWorkerRegistration.register(config);
-
+// dshfhsifuhsriufgher
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
